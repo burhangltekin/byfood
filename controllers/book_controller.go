dc692925328a63b43ae3f4770603a291867ac2dc
burhangltekin <burhangltekin2@gmail.com>
2025-06-12 11:47:01 +0300

    controllers

---
+++ controllers/book_controller.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/burhangltekin/byfood/models"
+	"github.com/burhangltekin/byfood/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func GetBooks(c *gin.Context) {
+	var books []models.Book
+	utils.DB.Find(&books)
+	c.JSON(http.StatusOK, books)
+}
+
+func GetBook(c *gin.Context) {
+	id := c.Param("id")
+	var book models.Book
+	if err := utils.DB.First(&book, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+	c.JSON(http.StatusOK, book)
+}
+
+func CreateBook(c *gin.Context) {
+	var book models.Book
+	if err := c.ShouldBindJSON(&book); err != nil || book.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+	utils.DB.Create(&book)
+	c.JSON(http.StatusCreated, book)
+}
+
+func UpdateBook(c *gin.Context) {
+	id := c.Param("id")
+	var book models.Book
+	if err := utils.DB.First(&book, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+	var input models.Book
+	if err := c.ShouldBindJSON(&input); err != nil || input.Title == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+	book.Title = input.Title
+	book.Author = input.Author
+	book.Year = input.Year
+	utils.DB.Save(&book)
+	c.JSON(http.StatusOK, book)
+}
+
+func DeleteBook(c *gin.Context) {
+	id := c.Param("id")
+	utils.DB.Delete(&models.Book{}, id)
+	c.JSON(http.StatusOK, gin.H{"message": "Book deleted"})
+}
